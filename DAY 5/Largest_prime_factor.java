7508d718144593417e60ad6eb9dc691ef76e5c72
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-05 20:34:44 +0530

    Add files via upload

---
+++ DAY 5/Largest_prime_factor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Largest_prime_factor {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int num=sc.nextInt();
+        int largestPrimeFactor=1;
+        for(int i=2;i<=num;i++){
+            if(num%i==0){
+                boolean isPrime=true;
+                for(int j=2;j<=i/2;j++){
+                    if(i%j==0){
+                        isPrime=false;
+                        break;
+                    }
+                }
+                if(isPrime){
+                    largestPrimeFactor=i;
+                }
+            }
+        }
+        System.out.println("Largest Prime Factor is: " + largestPrimeFactor);
+        sc.close();
+    }
+}
