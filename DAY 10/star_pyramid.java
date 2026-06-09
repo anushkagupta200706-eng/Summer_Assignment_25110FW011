a254bddfdc48f65059a1697988718f703ea21585
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-10 01:29:28 +0530

    Print star pyramid pattern

---
+++ DAY 10/star_pyramid.java
@@ -0,0 +1,15 @@
+public class star_pyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            //spaces
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }   
+}
