b09886a1b54ac43c4cd3fa88880618c67fa4e0ee
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-11 12:27:31 +0530

    Create a function to check prime

---
+++ DAY  11/prime_function.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class prime_function {
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the number: ");
+        int num=sc.nextInt();
+        if(isPrime(num)){
+            System.out.println("Prime number");
+        }else{
+            System.out.println("Not a prime number");
+        }
+        sc.close();
+    }
+}
