c323d48eaca55bb15ce9d797f1778606fffbb478
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-17 19:25:38 +0530

    Merge arrays

---
+++ DAY 17/MergeArrays.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class MergeArrays {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("enter size of first array: ");
+        int n1=sc.nextInt();
+        int arr1[]=new int[n1];
+
+        System.out.println("enter array elements: ");
+        for(int i=0;i<n1;i++){
+            arr1[i]=sc.nextInt();
+        }
+
+        System.out.print("enter size of second array: ");
+        int n2=sc.nextInt();
+        int arr2[]=new int[n1];
+
+        System.out.println("enter array elements: ");
+        for(int i=0;i<n2;i++){
+            arr2[i]=sc.nextInt();
+        }
+        
+        int merge[]=new int[n1+n2];
+
+        for(int i=0;i<n1;i++){
+            merge[i]=arr1[i];
+        }
+        for(int i=0;i<n2;i++){
+            merge[n1+i]=arr2[i];
+        }
+
+        System.out.println("Merged Array is: ");
+        for(int i=0;i<n1+n2;i++){
+            System.out.print(merge[i] + " ");
+        }
+        sc.close();
+    }
+}
