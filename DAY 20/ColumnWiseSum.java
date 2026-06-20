7e8ff0b264295c764f4529a40dc677cb6e60b450
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-20 19:27:43 +0530

    Column Wise Sum

---
+++ DAY 20/ColumnWiseSum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ColumnWiseSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter rows: ");
+        int r=sc.nextInt();
+        System.out.println("enter columns: ");
+        int c=sc.nextInt();
+
+        int a[][]=new int[r][c];
+
+        System.out.println("enter matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+        
+        for(int j=0;j<c;j++){
+            int sum=0;
+            for(int i=0;i<r;i++){
+                sum+=a[i][j];
+            }
+            System.out.println("Sum of Row " + (j+1) + " = " + sum);
+        }
+        sc.close();
+    }
+}
