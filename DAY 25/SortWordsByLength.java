468666faee120a02450ff2b6d2d6a9fa1e89e928
Anushka Gupta <anushka.gupta200706@gmail.com>
2026-06-25 22:05:00 +0530

    Sort words by length

---
+++ DAY 25/SortWordsByLength.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class SortWordsByLength {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence=sc.nextLine();
+
+        String words[]=sentence.split(" ");
+        
+        for(int i=0; i<words.length-1;i++) {
+            for(int j=i+1;j<words.length;j++) {
+                if(words[i].length() > words[j].length()) {
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Words sorted by length:");
+        for(int i=0;i<words.length;i++) {
+            System.out.println(words[i]);
+        }
+        sc.close();
+    }
+}
